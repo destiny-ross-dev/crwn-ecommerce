fcdea3b94feb0067b8cdc38e72a8302f42ee2434
Destiny Ross <destinylaross@gmail.com>
2024-02-24 08:34:26 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./categories.styles.scss";
+import CategoryMenu from "./components/category-menu/category-menu.component";
+
+const App = () => {
+  return <CategoryMenu />;
+};
+
+export default App;
