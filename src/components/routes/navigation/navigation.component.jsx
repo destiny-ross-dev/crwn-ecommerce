77935f948ccad9c0f8612ad600ee2652b34e241c
Destiny Ross <destinyross.dev@gmail.com>
2024-02-24 19:24:39 -0500

    refactored authentication code with usercontext

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,8 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
+
+import { UserContext } from "../../../contexts/user.context";
+
 import CrwnLogo from "../../../assets/crown.svg?react";
 import "./navigation.styles.scss";
+import { useContext } from "react";
+import { signOutUser } from "../../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    const res = await signOutUser();
+    console.log(res);
+  };
+
+  console.log(currentUser);
   return (
     <>
       <div className="navigation">
@@ -13,9 +26,15 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/auth">
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutHandler}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </nav>
       </div>
       <Outlet />
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,29 +1,28 @@
 import { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
-import FormInput from "../form-input/form-input.component";
+
 import "./sign-up-form.styles.scss";
-import Button from "../button/button.component";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const SignUpForm = () => {
-  const defaultFormFields = {
-    displayName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  };
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormFields({ ...formFields, [name]: value });
   };
 
   const handleSubmit = async (event) => {
@@ -44,56 +43,59 @@ const SignUpForm = () => {
       resetFormFields();
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
-        alert("Cannot create user, email already in use.");
-      } else if (error.code == "auth/weak-password") {
-        alert("Weak password: Password should be more than 6 characters.");
+        alert("Cannot create user, email already in use");
       } else {
         console.log("user creation encountered an error", error);
       }
     }
   };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     <div className="sign-up-container">
       <h2>{`Don't have an account?`}</h2>
-      <span>Sign Up With Email and Password</span>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
           type="text"
+          required
           onChange={handleChange}
           name="displayName"
-          required
           value={displayName}
         />
 
         <FormInput
           label="Email"
           type="email"
+          required
           onChange={handleChange}
           name="email"
           value={email}
-          required
         />
 
         <FormInput
           label="Password"
           type="password"
+          required
           onChange={handleChange}
           name="password"
           value={password}
-          required
         />
 
         <FormInput
           label="Confirm Password"
           type="password"
+          required
           onChange={handleChange}
           name="confirmPassword"
           value={confirmPassword}
-          required
         />
-
         <Button type="submit">Sign Up</Button>
       </form>
     </div>
